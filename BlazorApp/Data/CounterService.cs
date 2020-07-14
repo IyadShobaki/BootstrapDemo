60d1947dbb4e55a7ae786f3bc2e2e6690161d604
IyadShobaki <35826354+IyadShobaki@users.noreply.github.com>
2020-07-14 19:26:06 -0400

    Initial commit

---
+++ BlazorApp/Data/CounterService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Data
+{
+    public class CounterService : ICounterService
+    {
+        private readonly ILogger<CounterService> _log;
+
+        public int CounterValue { get; private set; }
+
+        public CounterService(ILogger<CounterService> log)
+        {
+            _log = log;
+        }
+
+        public void IncrementCounter()
+        {
+            CounterValue += 1;
+            _log.LogInformation("The counter was incremented to {CounterValue}", CounterValue);
+        }
+    }
+}
